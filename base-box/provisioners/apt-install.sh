a1695f7ed5fee4c96897f188186dfc77d91ed906
Vagrant Default User <pedroliborioln@gmail.com>
2022-07-23 21:56:05 +0000

    adding provisioners

---
+++ base-box/provisioners/apt-install.sh
@@ -0,0 +1,25 @@
+sudo apt-get update
+sudo apt-get -y install gcc g++ python
+sudo apt-get -y install gcc g++ python python-dev
+sudo apt-get -y install qt4-dev-tools libqt4-dev
+sudo apt-get -y install mercurial
+sudo apt-get -y install bzr
+sudo apt-get -y install cmake libc6-dev libc6-dev-i386 g++-multilib
+sudo apt-get -y install gdb valgrind
+sudo apt-get -y install gsl-bin libgsl2 libgsl2:i386
+sudo apt-get -y install flex bison libfl-dev
+sudo apt-get -y install tcpdump
+sudo apt-get -y install sqlite sqlite3 libsqlite3-dev
+sudo apt-get -y install libxml2 libxml2-dev
+sudo apt-get -y install libgtk2.0-0 libgtk2.0-dev
+sudo apt-get -y install vtun lxc
+sudo apt-get -y install git
+sudo apt-get -y install python
+sudo apt-get -y install python3
+sudo apt-get -y install python3-pip
+sudo apt-get -y install python3 -m pip install matplotlib
+sudo apt-get -y install python3 -m pip install numpy
+sudo apt-get -y install python3 -m pip install scipy
+apt-get update
+sudo apt-get -y install build-essential module-assistant dkms
+sudo apt-get -y install linux-image-amd64 linux-headers-amd64
